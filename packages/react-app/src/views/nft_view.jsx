bc34da67498cc440b8b7329de81bafca7312c396
Oriojas <oscarriojas@gmail.com>
2022-09-17 23:20:11 -0500

    create NFT with p5.js

--- packages/react-app/src/views/nft_view.jsx
+++ packages/react-app/src/views/nft_view.jsx
@@ -12,8 +12,11 @@ import Sketch from "react-p5";
  **/
 function NftExample() {
   // variables p5
-  let a = 300;
-  let b = 300;
+  let inc = 60;
+  let x = 200;
+  let y = 350;
+  let oldX = x;
+  let oldY = y;
 
   let setup = (p5, canvasParentRef) => {
     let xyz = p5.createCanvas(500, 400).parent(canvasParentRef);
@@ -21,15 +24,75 @@ function NftExample() {
     let x_grid = (p5.windowWidth - p5.width) / 2;
     let y_grid = (p5.windowHeight - p5.height) / 0;
     xyz.position(x_grid, y_grid);
+    p5.background(0, 0, 0);
+
+    p5.textSize(34);
+    p5.stroke(255, 90);
+    p5.strokeWeight(4);
+    p5.text("eth Bogotá", 165, 368);
   };
+
   let draw = p5 => {
-    p5.background("rgb(0%,0%,0%)");
+    p5.fill(255, 90);
+    p5.stroke(255, 255, 255, 40);
+    p5.strokeWeight(1);
+    p5.frameRate(24);
+
+    let rX = p5.random(-inc, inc);
+    let rY = p5.random(-inc, inc);
+    let avgInc = (rX + rY) / 10;
+
+    if (x < p5.width) {
+      x = x + rX;
+    } else {
+      x = oldX - inc;
+    }
+
+    if (x > 0) {
+      x = x + rX;
+    } else {
+      x = oldX + inc;
+    }
+
+    if (y < p5.height) {
+      y = y + rY;
+    } else {
+      y = oldY - inc;
+    }
+
+    if (y > 0) {
+      y = y + rY;
+    } else {
+      y = oldY + inc;
+    }
+
+    p5.fill(255, 90);
+    p5.ellipse(x, y, avgInc, avgInc);
+    p5.line(oldX, oldY, x, y);
+
+    oldX = x;
+    oldY = y;
+
+    p5.fill(0, 0, 0);
+    p5.stroke(255, 255, 255, 40);
+    p5.strokeWeight(2);
+    p5.triangle(175, 202, 250, 168, 250, 78);
+    p5.triangle(325, 202, 250, 168, 250, 78);
+    p5.triangle(175, 202, 250, 168, 250, 246);
+    p5.triangle(325, 202, 250, 168, 250, 246);
+    p5.triangle(175, 216, 250, 260, 250, 322);
+    p5.triangle(325, 216, 250, 260, 250, 322);
+
+    p5.fill(0, 0, 0, 60);
+    p5.noStroke();
+    p5.text("eth Bogotá", 165, 368);
   };
 
   return (
     <div>
-      <div style={{ margin: 32 }}>
-        <div className="canvas">
+      <br></br>
+      <div>
+        <div className="canvas" style={{ margin: "auto" }}>
           <Sketch setup={setup} draw={draw} className="canvas" />
         </div>
       </div>
