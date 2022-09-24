e40ceb2398fe8f1ae71b699889a4d3c255c4e86e
Oriojas <oscarriojas@gmail.com>
2022-09-23 22:50:00 -0500

    reaplce p5 react for eact-p5-wrapper

--- packages/react-app/src/views/nft_view.jsx
+++ packages/react-app/src/views/nft_view.jsx
@@ -2,7 +2,7 @@ import { useContractReader } from "eth-hooks";
 import { ethers } from "ethers";
 import React from "react";
 import { Link } from "react-router-dom";
-import Sketch from "react-p5";
+import { ReactP5Wrapper } from "react-p5-wrapper";
 import { Button } from "antd";
 import { Upload } from "../components";
 
@@ -20,82 +20,91 @@ function NftExample() {
   let oldX = x;
   let oldY = y;
 
-  let setup = (p5, canvasParentRef) => {
-    let xyz = p5.createCanvas(500, 400).parent(canvasParentRef);
+  function setup(p5) {
+    return () => {
+      let xyz = p5.createCanvas(500, 400, p5.SVG);
 
-    let x_grid = (p5.windowWidth - p5.width) / 2;
-    let y_grid = (p5.windowHeight - p5.height) / 0;
-    xyz.position(x_grid, y_grid);
-    p5.background(0, 0, 0);
+      let x_grid = (p5.windowWidth - p5.width) / 2;
+      let y_grid = (p5.windowHeight - p5.height) / 0;
+      xyz.position(x_grid, y_grid);
+      p5.background(0, 0, 0);
 
-    p5.textFont("Courier");
-    p5.textSize(36);
-    p5.stroke(255, 90);
-    p5.strokeWeight(4);
-    p5.text("eth Bogotá", 140, 368);
-  };
+      p5.textFont("Courier");
+      p5.textSize(36);
+      p5.stroke(255, 90);
+      p5.strokeWeight(4);
+      p5.text("eth Bogotá", 140, 368);
+    };
+  }
 
-  let draw = p5 => {
-    p5.fill(255, 90);
-    p5.stroke(255, 255, 255, 40);
-    p5.strokeWeight(1);
-    p5.frameRate(24);
+  function draw(p5) {
+    return () => {
+      p5.fill(255, 90);
+      p5.stroke(255, 255, 255, 40);
+      p5.strokeWeight(1);
+      p5.frameRate(18);
 
-    let rX = p5.random(-inc, inc);
-    let rY = p5.random(-inc, inc);
-    let avgInc = (rX + rY) / 10;
+      let rX = p5.random(-inc, inc);
+      let rY = p5.random(-inc, inc);
+      let avgInc = (rX + rY) / 10;
 
-    if (x < p5.width) {
-      x = x + rX;
-    } else {
-      x = oldX - inc;
-    }
+      if (x < p5.width) {
+        x = x + rX;
+      } else {
+        x = oldX - inc;
+      }
 
-    if (x > 0) {
-      x = x + rX;
-    } else {
-      x = oldX + inc;
-    }
+      if (x > 0) {
+        x = x + rX;
+      } else {
+        x = oldX + inc;
+      }
 
-    if (y < p5.height) {
-      y = y + rY;
-    } else {
-      y = oldY - inc;
-    }
+      if (y < p5.height) {
+        y = y + rY;
+      } else {
+        y = oldY - inc;
+      }
 
-    if (y > 0) {
-      y = y + rY;
-    } else {
-      y = oldY + inc;
-    }
+      if (y > 0) {
+        y = y + rY;
+      } else {
+        y = oldY + inc;
+      }
 
-    p5.fill(255, 90);
-    p5.ellipse(x, y, avgInc, avgInc);
-    p5.line(oldX, oldY, x, y);
+      p5.fill(255, 90);
+      p5.ellipse(x, y, avgInc, avgInc);
+      p5.line(oldX, oldY, x, y);
 
-    oldX = x;
-    oldY = y;
+      oldX = x;
+      oldY = y;
 
-    p5.fill(0, 0, 0);
-    p5.stroke(255, 255, 255, 40);
-    p5.strokeWeight(2);
-    p5.triangle(175, 202, 250, 168, 250, 78);
-    p5.triangle(325, 202, 250, 168, 250, 78);
-    p5.triangle(175, 202, 250, 168, 250, 246);
-    p5.triangle(325, 202, 250, 168, 250, 246);
-    p5.triangle(175, 216, 250, 260, 250, 322);
-    p5.triangle(325, 216, 250, 260, 250, 322);
+      p5.fill(0, 0, 0);
+      p5.stroke(255, 255, 255, 40);
+      p5.strokeWeight(2);
+      p5.triangle(175, 202, 250, 168, 250, 78);
+      p5.triangle(325, 202, 250, 168, 250, 78);
+      p5.triangle(175, 202, 250, 168, 250, 246);
+      p5.triangle(325, 202, 250, 168, 250, 246);
+      p5.triangle(175, 216, 250, 260, 250, 322);
+      p5.triangle(325, 216, 250, 260, 250, 322);
 
-    p5.fill(0, 0, 0, 60);
-    p5.noStroke();
-    p5.textFont("Courier");
-    p5.text("eth Bogotá", 140, 368);
+      p5.fill(0, 0, 0, 60);
+      p5.noStroke();
+      p5.textFont("Courier");
+      p5.text("eth Bogotá", 140, 368);
 
-    if (p5.mouseIsPressed) {
-      p5.save("Created_NFT.png");
-      p5.noLoop();
-    }
-  };
+      if (p5.mouseIsPressed) {
+        p5.save("Created_NFT.png");
+        p5.noLoop();
+      }
+    };
+  }
+
+  function sketch(p5) {
+    p5.setup = setup(p5);
+    p5.draw = draw(p5);
+  }
 
   return (
     <div>
@@ -103,7 +112,7 @@ function NftExample() {
         <br></br>
         <div>
           <div className="canvas" style={{ margin: "auto" }}>
-            <Sketch setup={setup} draw={draw} className="canvas" />
+            <ReactP5Wrapper sketch={sketch} />;
           </div>
         </div>
       </div>
